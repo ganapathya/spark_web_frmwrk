a840ef4cca192a0e4f5f6171ae6a76a233f8ac8d
Ganapathy <ggs_aiyer@gmail.com>
2016-09-19 16:01:00 +0530

    Renamed SparServer to ChatServer and ran a couple of builds

--- src/main/com/myspark/server/ChatWebSocketHandler.java
+++ src/main/com/myspark/server/ChatWebSocketHandler.java
@@ -3,6 +3,8 @@ package com.myspark.server;
 import org.eclipse.jetty.websocket.api.*;
 import org.eclipse.jetty.websocket.api.annotations.*;
 
+
+
 /**
  * Created by ganapathys on 9/19/2016.
  */
@@ -12,21 +14,21 @@ public class ChatWebSocketHandler {
 
     @OnWebSocketConnect
     public void onConnect(Session user) throws Exception {
-        String username = "User" + SparkServer.nextUserNumber++;
-        SparkServer.userUsernameMap.put(user, username);
-        SparkServer.broadcastMessage(sender = "Server", msg = (username + " joined the chat"));
+        String username = "User" + ChatServer.nextUserNumber++;
+        ChatServer.userUsernameMap.put(user, username);
+        ChatServer.broadcastMessage(sender = "Server", msg = (username + " joined the chat"));
     }
 
     @OnWebSocketClose
     public void onClose(Session user, int statusCode, String reason) {
-        String username = SparkServer.userUsernameMap.get(user);
-        SparkServer.userUsernameMap.remove(user);
-        SparkServer.broadcastMessage(sender = "Server", msg = (username + " left the chat"));
+        String username = ChatServer.userUsernameMap.get(user);
+        ChatServer.userUsernameMap.remove(user);
+        ChatServer.broadcastMessage(sender = "Server", msg = (username + " left the chat"));
     }
 
     @OnWebSocketMessage
     public void onMessage(Session user, String message) {
-        SparkServer.broadcastMessage(sender = SparkServer.userUsernameMap.get(user), msg = message);
+        ChatServer.broadcastMessage(sender = ChatServer.userUsernameMap.get(user), msg = message);
     }
 
 
